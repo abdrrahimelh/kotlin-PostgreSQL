8a04bbd601e29a6eff9f836e96abd2a187048132
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2022-03-06 15:43:45 +0000

    first

--- src/main/kotlin/inpt/sud/springbackkot/dao/CategoryRepository.kt
+++ src/main/kotlin/inpt/sud/springbackkot/dao/CategoryRepository.kt
@@ -3,7 +3,8 @@ package inpt.sud.springbackkot.dao
 import inpt.sud.springbackkot.data.Category
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin("*")
 @RepositoryRestResource
 
 interface CategoryRepository : JpaRepository<Category?, Long?>
--- src/main/kotlin/inpt/sud/springbackkot/dao/ProductRepository.kt
+++ src/main/kotlin/inpt/sud/springbackkot/dao/ProductRepository.kt
@@ -3,6 +3,8 @@ package inpt.sud.springbackkot.dao
 import inpt.sud.springbackkot.data.Product
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @RepositoryRestResource
 interface ProductRepository : JpaRepository<Product?, Long?>
